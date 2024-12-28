ef590fbfeceb52f01d8868f1b3bd05431076bb15
shaku-hussien <nadahussein686@gmail.com>
2024-12-28 10:57:55 +0300

    day 26

---
+++ main.js
@@ -0,0 +1,57 @@
+                                  //switch    
+    
+    //     let price = prompt("what do you want")
+    //     let value = ( price)
+    //     console.log(value)
+    //     switch(price) {
+    //     case "apple":
+    //     console.log("apple peice is: $1")
+    //     break;
+    //     case"mango":
+    //     console.log("mango peice is: $2")
+    //     break;
+    //     case "orange":
+    //     console.log("orange peice is: $3")
+    //     break;
+    //     default:
+    //     console.log("ma hayno waxa aad rabto!") 
+
+    // }
+
+                                     // for loop
+
+                                     
+    // for (var a = 0;   a <= 100;   a++) {
+    //     console.log(a)
+    // }
+
+    // for(let n = 0; n<=30; n+=2){
+    //     console.log(n)
+    // }
+
+
+                                         //while loop
+
+    // let num = 1;
+    // let num = 1 
+    // while(num <= 50){
+    //     console.log(num)
+    //     console.log(num)
+    //     num++;
+    // }
+
+// let value = 1;
+// while (value  <= 30){
+//     console.log(value)
+//     value += 2;
+// }
+
+
+// do while loop
+
+let value = 200;
+do{
+    console.log(value)
+    value--;
+}
+while(value >= 1);
